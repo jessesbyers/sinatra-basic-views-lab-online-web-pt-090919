958ce2e3f5e29ca843ae68eb0de3066328eb5d57
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-02 15:11:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-
+  get '/' do
+		erb :index
+	end
 end
+
+# W
+# App
+#   GET '/'
+#     returns a 200 status code (FAILED - 1)
+#     renders index.erb (FAILED - 2)
